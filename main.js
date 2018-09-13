9b7daad5ea9508b444ffdc00d202df8bca14adb7
Shana Opperman <shanaopperman@Shanas-MacBook-Pro.local>
2018-09-13 15:56:34 -0400

    my solution

--- main.js
+++ main.js
@@ -2,3 +2,15 @@
 //----------------------------------
 //          YOUR CODE
 //----------------------------------
+var counter = 0; 
+
+for (var i= 0; i < 81; i++) {
+	
+	y = j*15; 
+	
+	for (var j = 0; j < 53; j++) {
+		x = i*15; 
+		create(x,y,colors[counter]);
+		counter++;
+	}
+}
